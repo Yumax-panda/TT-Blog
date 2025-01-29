bcce4c5a4381714964b64199899f2ff9bbbdd042
Yumax-panda <yuma0wall@gmail.com>
2025-01-30 03:08:58 +0900

    vue-routerを導入

---
+++ web/src/router/index.ts
@@ -0,0 +1,33 @@
+import type { RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+
+export enum RouteName {
+  Index = 'index'
+}
+
+const MainPage = () => import('../views/MainPage.vue')
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: RouteName.Index,
+    component: MainPage
+  }
+]
+
+const routerHistory = createWebHistory(import.meta.env.BASE_URL)
+
+const router = createRouter({
+  history: routerHistory,
+  routes
+})
+
+router.beforeEach(to => {
+  // trailing slashを消す
+  if (to.path !== '/' && to.path.endsWith('/')) {
+    return to.path.slice(0, -1)
+  }
+  return true
+})
+
+export default router
