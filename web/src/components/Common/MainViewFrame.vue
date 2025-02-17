e8aa53c122aa209556c9141ca1bd3e9765876206
Yumax-panda <yuma0wall@gmail.com>
2025-02-17 19:07:10 +0900

    冗長なファイルを削除

--- web/src/components/Common/MainViewFrame.vue
+++ web/src/components/Common/MainViewFrame.vue
@@ -1,17 +1,14 @@
 <template>
   <main-header />
   <div :class="$style.container">
-    <welcome-section />
-    <main-section />
+    <slot></slot>
   </div>
   <main-footer />
 </template>
 
 <script lang="ts" setup>
-import MainSection from './MainSection/MainSection.vue'
 import MainFooter from './MainFooter.vue'
 import MainHeader from './MainHeader.vue'
-import WelcomeSection from './WelcomeSection.vue'
 </script>
 
 <style lang="scss" module>
--- web/src/views/MainPage.vue
+++ web/src/views/MainPage.vue
@@ -1,7 +1,18 @@
 <template>
-  <main-view />
+  <main-view-frame>
+    <welcome-section />
+    <main-section />
+  </main-view-frame>
 </template>
 
 <script lang="ts" setup>
-import MainView from '/@/components/Main/MainView.vue'
+import MainViewFrame from '/@/components/Common/MainViewFrame.vue'
+import MainSection from '/@/components/Main/MainSection/MainSection.vue'
+import WelcomeSection from '/@/components/Main/WelcomeSection/WelcomeSection.vue'
 </script>
+
+<style lang="scss" module>
+.container {
+  min-height: 100vh;
+}
+</style>
