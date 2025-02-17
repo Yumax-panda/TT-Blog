3159fff92c124a3052db45bf4984bdb1bebdacf2
Yumax-panda <yuma0wall@gmail.com>
2025-02-17 23:06:17 +0900

    コンテンツの外枠を共通コンポーネントとして切り出した

---
+++ web/src/components/Common/ContentWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="$style.wrapper">
+    <div :class="$style.content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" module>
+.wrapper {
+  @include content-wrapper;
+}
+
+.content {
+  margin: 5rem auto;
+  width: 80%;
+}
+</style>
